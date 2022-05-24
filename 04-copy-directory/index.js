aef60f38180a6f990512e87c21c3b0ae0431a214
yuliya <julia.one.such@gmail.com>
2022-05-24 18:34:22 +0300

    fix: copied folder updated

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,13 +4,20 @@ const folder = path.join(__dirname, 'files');
 const newFolder = path.join(__dirname, 'files-copy');
 
 async function copy() {
+  if (fsPromises.stat(newFolder)) {
+    const files =  await fsPromises.readdir(newFolder);
+    for (let i = 0; i < files.length; i++) {
+      await fsPromises.rm(newFolder + '/' + files[i]);
+    }
+    await fsPromises.rmdir(newFolder);
+  }
   fsPromises.mkdir(newFolder, { recursive: true }, (err) => {
     if (err) throw err;
   });
-  const files = await fsPromises.readdir(folder);
+  let files = await fsPromises.readdir(folder);
   for(let i = 0; i < files.length; i++){
-    const srcFile = path.join(folder, files[i]);
-    const destFile = path.join(newFolder, files[i]);
+    let srcFile = path.join(folder, files[i]);
+    let destFile = path.join(newFolder, files[i]);
     fsPromises.copyFile(srcFile, destFile);
   }
 }
