48136646cddd1030ce75e17a20242922a4c655cf
yuliya <julia.one.such@gmail.com>
2022-05-25 17:16:17 +0300

    fix: fix checking by type

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -6,9 +6,10 @@ const bundle = path.join(project, 'bundle.css');
 
 async function write() {
   let arr = [];
+  const stylesTypes = await fsPromises.readdir(styles, {withFileTypes: true});
   const stylesFiles = await fsPromises.readdir(styles);
   for (let i = 0; i < stylesFiles.length; i++) {
-    if (stylesFiles[i].includes('css')) {
+    if (path.extname(stylesTypes[i].name) === '.css') {
       const content = await fsPromises.readFile(styles + '/' + stylesFiles[i], 'utf-8');
       arr.push(content);
     }
